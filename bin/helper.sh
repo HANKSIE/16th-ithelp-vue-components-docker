595e09985cdf5a970c47a9f04df2e7fd2ca542fd
HANKSIE <kkuuccff@gmail.com>
2024-09-19 07:27:57 +0800

    remove exit_if_err & safe_exit

--- bin/helper.sh
+++ bin/helper.sh
@@ -9,43 +9,23 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 COLOR_OFF='\033[0m'
 
-# 如果有錯誤就顯示錯誤, 沒錯誤就正常結束程序
-safe_exit(){
-    exit_if_err
-    # 正常結束程序
-    exit 0
-}
-
-# 如果有錯誤就顯示錯誤並結束程序
-exit_if_err(){
-    if [[ $? -ne 0 ]]
-    then
-        echo -e "${RED}fail${COLOR_OFF}"
-        # 非正常結束程序
-        exit 1
-    fi
-}
-
 # 顯示當前動作
 echo_action(){
     echo -e "${YELLOW}$1${COLOR_OFF}"
 }
 
 start_exit(){
-    exit_if_err
     echo -e "${GREEN}done${COLOR_OFF}"
     echo -e "open ${BLUE}http://localhost:$LOCAL_PORT${COLOR_OFF}"
-    safe_exit
+    exit 0
 }
 
 done_exit(){
-    exit_if_err
     echo -e "${GREEN}done${COLOR_OFF}"
-    safe_exit
+    exit 0
 }
 
 unknown_cmd_exit(){
-    exit_if_err
     echo -e "${BOLD}unknown cmd${COLOR_OFF}"
-    safe_exit
+    exit 0
 }
